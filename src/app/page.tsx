388b8e68706dfe7668da2536104c30b0e196fa63
Shuhua-L <sl.shuhualiu@gmail.com>
2023-08-02 17:46:40 -0400

    🎉 init: add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button className="btn">Hello, READMEXpress</button>
-    </main>
+    <button className="btn">Hello, READMEXpress</button>
+  </main>
   )
 }

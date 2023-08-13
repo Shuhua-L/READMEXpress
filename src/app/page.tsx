1fee24e4728619c76d08587b505f6c22bdc7a40d
Shuhua-L <sl.shuhualiu@gmail.com>
2023-08-12 22:11:13 -0400

    ✨ feat: table of contents
    added a function to generate table of contents dynamically

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import InputForms from "@/InputForms";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
@@ -14,11 +14,40 @@ export default function Home() {
 
   const [document, setDocument] = useState([""]);
 
+  // TODO: ugly but it's working!! refactor later
+  const generateTableOfContents = () => {
+    let doc = document.join("\n");
+
+    let matches = Array.from(doc.matchAll(/\n(#+)[ \t](.+)\n/gm));
+    let sizes = matches.map((groups) => groups[1].length);
+    let minSize = Math.min(...sizes);
+    sizes = sizes.map((size) => size - minSize);
+    let headings = matches.map((groups) => groups[2]);
+
+    const slugify = (str: string) =>
+      str
+        .toLowerCase()
+        .trim()
+        .replace(/[^\w\s-]/g, "")
+        .replace(/[\s_-]+/g, "-")
+        .replace(/^-+|-+$/g, "");
+
+    // console.log({ sizes, headings });
+
+    let TOC = `## Table of Contents`;
+    TOC += " \n";
+    headings.forEach((h, i) => {
+      let tabs = new Array(sizes[i]).fill("\t").join("");
+      TOC += tabs + `* [${h}](#${slugify(h)}) \n`;
+    });
+    return TOC;
+  };
+
   const updateDocument = (doc: string) => {
     setDocument([...document, doc]);
   };
 
-  // console.log(document.join("\n"));
+  console.log(generateTableOfContents() + document.join("\n"));
 
   return (
     <div className='px-4 gap-4 md:flex flex-auto'>
@@ -27,7 +56,7 @@ export default function Home() {
       </div>
       <div className='bg-accent-focus md:w-1/2 mb-[1vh] max-h-[92vh] min-h-[45vh] md:overflow-y-scroll'>
         <ReactMarkdown rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]} className='prose'>
-          {document.join("\n")}
+          {generateTableOfContents() + document.join("\n")}
         </ReactMarkdown>
       </div>
     </div>
